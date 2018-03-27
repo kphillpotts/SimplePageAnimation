088871499e1ac605356708f07c6b9a203cad2803
Kym Phillpotts <kphillpotts@gmail.com>
2018-03-27 11:30:45 +1100

    Initial project commit
    Just the basics

---
+++ Animation/AnimationPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Animation
+{
+    public partial class AnimationPage : ContentPage
+    {
+        public AnimationPage()
+        {
+            InitializeComponent();
+        }
+
+        bool isExpanded = true;
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            if (isExpanded)
+                AnimateIn();
+            else
+                AnimateOut();
+
+            isExpanded = !isExpanded;
+        }
+
+        Rectangle expandedRect;
+        Rectangle detailsRect;
+
+        private void AnimateIn()
+        {
+            MainImage.LayoutTo(detailsRect, 1200, Easing.SpringOut);
+            BottomFrame.TranslateTo(0, 0, 1200, Easing.SpringOut);
+            Title.TranslateTo(0, 0, 1200, Easing.SpringOut);
+            ExpandBar.FadeTo(.01, 250, Easing.SinInOut);
+        }
+
+        private void AnimateOut()
+        {
+            MainImage.LayoutTo(expandedRect, 1200, Easing.SpringOut);
+            BottomFrame.TranslateTo(0, BottomFrame.Height, 1200, Easing.SpringOut);
+            Title.TranslateTo(-Title.Width, 0, 1200, Easing.SpringOut);
+            ExpandBar.FadeTo(1, 250, Easing.SinInOut);
+        }
+
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            detailsRect = new Rectangle(0, 0, width, BottomFrame.Bounds.Top + 20);
+            expandedRect = new Rectangle(0, 0, width, height);
+
+            if (isExpanded)
+            {
+                MainImage.Layout(expandedRect);
+                BottomFrame.TranslationY = BottomFrame.Height;
+                Title.TranslationX = -Title.Width;
+            }
+            else
+            {
+                MainImage.Layout(detailsRect);
+                BottomFrame.TranslationY = 0;
+                Title.TranslationX = 0;
+            }
+        }
+    }
+}
---
+++ Animation/Views/Title.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Animation.Views
+{
+    public partial class Title : ContentView
+    {
+        public Title()
+        {
+            InitializeComponent();
+        }
+    }
+}
